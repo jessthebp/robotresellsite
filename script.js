75a84a980d562f16eee4e9289b363edb38d751ac
Jess <jessbpeck@gmail.com>
2019-08-20 15:00:45 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+/* set the width of the side navigation to 300px */
+function openNav() {
+ document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
